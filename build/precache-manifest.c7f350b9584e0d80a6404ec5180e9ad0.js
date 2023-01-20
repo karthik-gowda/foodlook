ab2a1f9bbba04fe5b18cef9510d69e85411a16ae
Karthik BT <79859147+karthik-gowda@users.noreply.github.com>
2023-01-20 13:46:57 +0530

    Add files via upload

---
+++ build/precache-manifest.c7f350b9584e0d80a6404ec5180e9ad0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54631472cfb7f02a206ad1d04a5e08d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7fec022d3fc640459cf",
+    "url": "/static/css/2.3cb01340.chunk.css"
+  },
+  {
+    "revision": "7a827ba524c33f48d593",
+    "url": "/static/css/main.7d1af5fd.chunk.css"
+  },
+  {
+    "revision": "b7fec022d3fc640459cf",
+    "url": "/static/js/2.6261c38c.chunk.js"
+  },
+  {
+    "revision": "115c29c453b61995b5be0af8c45bd83e",
+    "url": "/static/js/2.6261c38c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a827ba524c33f48d593",
+    "url": "/static/js/main.6af89e78.chunk.js"
+  },
+  {
+    "revision": "405a0d9530fd470eb51a",
+    "url": "/static/js/runtime-main.e562068f.js"
+  },
+  {
+    "revision": "4b3603478f885be421831cccf3782ac0",
+    "url": "/static/media/bg.4b360347.jpg"
+  },
+  {
+    "revision": "c40966aa7a5c5a42855704701a701a52",
+    "url": "/static/media/foodimage.c40966aa.jpg"
+  },
+  {
+    "revision": "b213dc57bfa0ff88ca7522139c937acf",
+    "url": "/static/media/spinning-loading.b213dc57.gif"
+  }
+]);
